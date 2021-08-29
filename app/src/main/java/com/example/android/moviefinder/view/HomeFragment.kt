e3e614128ccd37a8fa161aee886a2aaa1605a689
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-08-29 15:35:08 +0300

    Создал шаблон для категории фильмов. Создал метод для создания категории и немного изменил показ состояний. Также внес небольшие правки и добавил русскую локализацию.

--- app/src/main/java/com/example/android/moviefinder/view/HomeFragment.kt
+++ app/src/main/java/com/example/android/moviefinder/view/HomeFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android.moviefinder.R
+import com.example.android.moviefinder.databinding.CategorySectionBinding
 import com.example.android.moviefinder.databinding.HomeFragmentBinding
 import com.example.android.moviefinder.viewmodel.AppState
 import com.example.android.moviefinder.viewmodel.HomeViewModel
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -20,7 +24,7 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
     private var _binding: HomeFragmentBinding? = null
     private val binding get() = _binding!!
-    private val firstSectionAdapter = MoviesAdapter()
+    private val firstCategoryAdapter = MoviesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,16 +36,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.firstSectionRecycler.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        binding.firstSectionRecycler.adapter = firstSectionAdapter
-
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        viewModel.liveData.observe(viewLifecycleOwner, { state ->
-            renderData(state, firstSectionAdapter)
-        })
-        viewModel.getFirstSectionMovies()
+        createCategory("Category 1", firstCategoryAdapter, viewModel.firstCategoryLiveData)
+        createCategory("Category 2", firstCategoryAdapter, viewModel.firstCategoryLiveData)
+        createCategory("Category 3", firstCategoryAdapter, viewModel.firstCategoryLiveData)
     }
 
     override fun onDestroyView() {
@@ -49,27 +47,43 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun renderData(state: AppState, adapter: MoviesAdapter) = with(binding) {
-        when (state) {
-            is AppState.Loading -> {
-                firstSectionRecycler.visibility = View.GONE
-                errorContainer.visibility = View.GONE
-                firstSectionLoading.visibility = View.VISIBLE
-            }
-            is AppState.Success -> {
-                firstSectionLoading.visibility = View.GONE
-                errorContainer.visibility = View.GONE
-                firstSectionRecycler.visibility = View.VISIBLE
-                adapter.setData(state.movies)
-            }
-            is AppState.Error -> {
-                firstSectionRecycler.visibility = View.GONE
-                firstSectionLoading.visibility = View.GONE
-                errorMessage.text = "Ошибка: ${state.error.message}"
-                errorButton.setOnClickListener {
-                    viewModel.getFirstSectionMovies()
+    private fun createCategory(title: String, adapter: MoviesAdapter, liveData: LiveData<AppState>) {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_section, binding.root, false)
+        binding.container.addView(view)
+        val sectionBinding = CategorySectionBinding.bind(view)
+        sectionBinding.title.text = title
+        sectionBinding.recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        sectionBinding.recyclerView.adapter = adapter
+        liveData.observe(viewLifecycleOwner) {
+            renderData(it, adapter, view)
+        }
+        viewModel.getFirstSectionMovies()
+    }
+
+    private fun renderData(state: AppState, adapter: MoviesAdapter, view: View) {
+        val sectionBinding = CategorySectionBinding.bind(view)
+        sectionBinding.apply {
+            when (state) {
+                is AppState.Loading -> {
+                    recyclerView.visibility = View.GONE
+                    errorLinear.visibility = View.GONE
+                    loadingTextView.visibility = View.VISIBLE
                 }
-                errorContainer.visibility = View.VISIBLE
+                is AppState.Success -> {
+                    errorLinear.visibility = View.GONE
+                    loadingTextView.visibility = View.GONE
+                    recyclerView.visibility = View.VISIBLE
+                    adapter.setData(state.movies)
+                }
+                is AppState.Error -> {
+                    recyclerView.visibility = View.GONE
+                    loadingTextView.visibility = View.GONE
+                    errorLinear.visibility = View.VISIBLE
+                    errorTextView.text = String.format(Locale.getDefault(), "%s: %s", resources.getString(R.string.error), state.error.message)
+                    errorButton.setOnClickListener {
+                        viewModel.getFirstSectionMovies()
+                    }
+                }
             }
         }
     }
