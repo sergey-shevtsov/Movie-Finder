b634c6ccfac2131c41ef9cdf7901a76f325c2108
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-09-06 20:04:09 +0300

    Убрал все значения по умолчанию из класса Movie

--- app/src/main/java/com/example/android/moviefinder/model/Movie.kt
+++ app/src/main/java/com/example/android/moviefinder/model/Movie.kt
@@ -6,16 +6,16 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Movie (
-    val id: Int = 1,
-    val imageId: Int = R.drawable.dummy,
-    val title: String = "Терминатор",
-    val originalTitle: String = "Terminator",
-    val genres: Array<String> = arrayOf("фантастика", "боевик", "триллер"),
-    val duration: Int = 108,
-    val rating: Float = 8.0f,
-    val voteCount: Int = 10000,
-    val budget: Long = 6_400_000,
-    val revenue: Long = 78_371_200,
-    val released: String = "29.10.1984",
-    val overview: String = "История противостояния солдата Кайла Риза и киборга-терминатора, прибывших в 1984-й год из пост-апокалиптического будущего, где миром правят машины-убийцы, а человечество находится на грани вымирания. Цель киборга: убить девушку по имени Сара Коннор, чей ещё нерождённый сын к 2029 году выиграет войну человечества с машинами. Цель Риза: спасти Сару и остановить Терминатора любой ценой."
-) : Parcelable
+    val id: Int,
+    val imageId: Int,
+    val title: String,
+    val originalTitle: String,
+    val genres: Array<String>,
+    val duration: Int,
+    val rating: Float,
+    val voteCount: Int,
+    val budget: Long,
+    val revenue: Long,
+    val released: String,
+    val overview: String
+    ) : Parcelable
