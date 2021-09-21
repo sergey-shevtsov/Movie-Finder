dfe6d5bad115e7d0d265e1ada51b475054cc95bd
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-09-21 22:04:37 +0300

    Понял, что данные в избранное нужно будет загружать из интернета и убрал все, что касается Room, оставил только класс App

--- app/src/main/java/com/example/android/moviefinder/model/local/LocalRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.moviefinder.model.local
-
-interface LocalRepository {
-    fun getAllFavorites(): List<FavoritesEntity>
-    fun addFavorite(favoritesEntity: FavoritesEntity)
-    fun deleteFavorite(favoritesEntity: FavoritesEntity)
-}
