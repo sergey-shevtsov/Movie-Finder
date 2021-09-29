f9ed3e0fba5ebb2557291d65aab0bee9af1f34a9
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-09-29 16:19:35 +0300

    Настроил получение разрешения и вывод контактов на экран

--- app/src/main/java/com/example/android/moviefinder/view/contacts/ContactsFragment.kt
+++ app/src/main/java/com/example/android/moviefinder/view/contacts/ContactsFragment.kt
@@ -1,13 +1,20 @@
 package com.example.android.moviefinder.view.contacts
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.moviefinder.databinding.ContactsFragmentBinding
+import com.example.android.moviefinder.model.PhonebookContact
 import com.example.android.moviefinder.view.showHomeButton
+import com.example.android.moviefinder.viewmodel.AppState
 import com.example.android.moviefinder.viewmodel.ContactsViewModel
 
 class ContactsFragment : Fragment() {
@@ -21,6 +28,24 @@ class ContactsFragment : Fragment() {
 
     private var _binding: ContactsFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val permissionResult =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            when {
+                result -> viewModel.getContacts(requireActivity())
+                !ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.READ_CONTACTS
+                ) -> {
+                    Toast.makeText(
+                        context,
+                        "Go to app settings and enable permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> Toast.makeText(context, "T_T", Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +62,32 @@ class ContactsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = ContactsAdapter()
+        initRecyclerView(adapter)
+        initViewModel(adapter)
 
+        permissionResult.launch(Manifest.permission.READ_CONTACTS)
+    }
+
+    private fun initRecyclerView(adapter: ContactsAdapter) {
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun initViewModel(adapter: ContactsAdapter) {
+        viewModel.liveData.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is AppState.Loading -> {
+
+                }
+                is AppState.Success<*> -> {
+                    adapter.setData(state.data as List<PhonebookContact>)
+                }
+                is AppState.Error -> {
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/moviefinder/viewmodel/ContactsViewModel.kt
+++ app/src/main/java/com/example/android/moviefinder/viewmodel/ContactsViewModel.kt
@@ -1,7 +1,71 @@
 package com.example.android.moviefinder.viewmodel
 
+import android.app.Activity
+import android.database.Cursor
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.moviefinder.model.PhonebookContact
 
-class ContactsViewModel : ViewModel() {
+class ContactsViewModel(
+    private val contactsLiveData: MutableLiveData<AppState> = MutableLiveData()
+) : ViewModel() {
+    val liveData: LiveData<AppState> = contactsLiveData
 
+    fun getContacts(activity: Activity) {
+        contactsLiveData.value = AppState.Loading
+
+        val cr = activity.contentResolver
+
+        val cursor: Cursor? = cr.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null, null, null, null
+        )
+
+        val contacts = mutableListOf<PhonebookContact>()
+
+        cursor?.let {
+            for (i in 0..cursor.count) {
+
+                if (cursor.moveToPosition(i)) {
+
+                    val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    var phoneNumber: String? = null
+
+                    if (cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
+                            .toInt() > 0
+                    ) {
+
+                        val phoneCursor = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} = ?",
+                            arrayOf(id),
+                            null
+                        )
+
+                        phoneCursor?.let {
+                            if (it.moveToFirst()) {
+                                phoneNumber =
+                                    it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                                it.close()
+                            }
+                        }
+
+                    }
+
+                    contacts.add(PhonebookContact(name, phoneNumber))
+
+                }
+
+            }
+
+            cursor.close()
+        }
+
+        contactsLiveData.postValue(AppState.Success(contacts))
+    }
 }
