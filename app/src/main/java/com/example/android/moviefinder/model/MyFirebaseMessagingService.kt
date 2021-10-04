edc190cbceaa73f5f6ad2a188b4ecdd843d1dbbc
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-04 14:59:51 +0300

    Добавил возможность получать уведомления через Firebase

---
+++ app/src/main/java/com/example/android/moviefinder/model/MyFirebaseMessagingService.kt
@@ -0,0 +1,79 @@
+package com.example.android.moviefinder.model
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.android.moviefinder.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val TAG = "MyFirebase"
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    companion object {
+        private const val PUSH_KEY_TITLE = "title"
+        private const val PUSH_KEY_MESSAGE = "message"
+        private const val CHANNEL_ID = "channel_id"
+        private const val NOTIFICATION_ID = 37
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val remoteMessageData = remoteMessage.data
+        if (remoteMessageData.isNotEmpty()) {
+            handleDataMessage(remoteMessageData.toMap())
+        }
+    }
+
+    private fun handleDataMessage(data: Map<String, String>) {
+        val title = data[PUSH_KEY_TITLE]
+        val message = data[PUSH_KEY_MESSAGE]
+        if (!title.isNullOrBlank() && !message.isNullOrBlank()) {
+            showNotification(title, message)
+        }
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val notificationBuilder =
+            NotificationCompat.Builder(applicationContext, CHANNEL_ID).apply {
+                setSmallIcon(R.drawable.ic_push_message)
+                setContentTitle(title)
+                setContentText(message)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val name = "News"
+        val descriptionText = "Important notifications"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "onNewToken = $token")
+    }
+}
