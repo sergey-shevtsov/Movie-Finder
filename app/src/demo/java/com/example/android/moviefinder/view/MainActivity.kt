afea0c23c3eae4004d07e9b88dbbee8f17b7cfc4
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-08 01:20:41 +0300

    Добавил суффикс для версии debug. Добавил новые версии приложения: demo и full. В demo функционал ограничен только главным экраном и детальным описанием фильма. В full функционал не урезан.

---
+++ app/src/demo/java/com/example/android/moviefinder/view/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.android.moviefinder.view
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.android.moviefinder.R
+import com.example.android.moviefinder.databinding.MainActivityBinding
+import com.example.android.moviefinder.model.*
+import com.example.android.moviefinder.view.contacts.ContactsFragment
+import com.example.android.moviefinder.view.favorites.FavoritesFragment
+import com.example.android.moviefinder.view.history.HistoryFragment
+import com.example.android.moviefinder.view.home.HomeFragment
+import com.example.android.moviefinder.view.location.LocationFragment
+import com.example.android.moviefinder.view.settings.SettingsFragment
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment.newInstance())
+        }
+
+        initNetworkCallback()
+    }
+
+    private val binding: MainActivityBinding by lazy {
+        MainActivityBinding.inflate(layoutInflater)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun initNetworkCallback() {
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(localMainReceiver, IntentFilter(NETWORK_STATUS_INTENT_FILTER))
+        NetworkMonitor(application).startNetworkCallback()
+    }
+
+    private val localMainReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.getStringExtra(NETWORK_STATUS)) {
+                AVAILABLE_STATUS -> {
+                    context?.let { onNetworkRestored(it) }
+                }
+                LOST_STATUS -> {
+                    context?.let { onNetworkLost(it) }
+                }
+            }
+        }
+    }
+
+    private fun onNetworkRestored(context: Context) {
+        Thread {
+            runOnUiThread {
+                binding.connectivityBar.apply {
+                    connectivityContainer.background =
+                        AppCompatResources.getDrawable(context, R.drawable.green_bg)
+                    connectivityTextView.text = getString(R.string.internet_restored)
+                }
+            }
+            Thread.sleep(2000)
+            runOnUiThread {
+                binding.connectivityBar.connectivityContainer.hide()
+            }
+        }.start()
+    }
+
+
+    private fun onNetworkLost(context: Context) {
+        binding.connectivityBar.apply {
+            connectivityContainer.show()
+            connectivityContainer.background =
+                AppCompatResources.getDrawable(context, R.drawable.red_bg)
+            connectivityTextView.text = getString(R.string.no_internet)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        NetworkMonitor(application).stopNetworkCallback()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+            else -> super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = false) {
+        supportFragmentManager.apply {
+            for (i in 0 until backStackEntryCount) popBackStack()
+            if (addToBackStack) {
+                beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack("")
+                    .commit()
+            } else {
+                beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+        }
+    }
+}
---
+++ app/src/demo/java/com/example/android/moviefinder/view/detail/DetailFragment.kt
@@ -0,0 +1,153 @@
+package com.example.android.moviefinder.view.detail
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import coil.api.load
+import com.example.android.moviefinder.R
+import com.example.android.moviefinder.databinding.DetailFragmentBinding
+import com.example.android.moviefinder.model.GenresDTO
+import com.example.android.moviefinder.model.MovieDetailsDTO
+import com.example.android.moviefinder.model.local.FavoritesEntity
+import com.example.android.moviefinder.model.local.HistoryEntity
+import com.example.android.moviefinder.view.*
+import com.example.android.moviefinder.viewmodel.AppState
+import com.example.android.moviefinder.viewmodel.DetailsViewModel
+import java.util.*
+
+const val DETAILS_URL_BASE = "https://image.tmdb.org/t/p/w300"
+
+class DetailFragment : Fragment() {
+
+    companion object {
+        const val MOVIE_ID_KEY = "MOVIE_EXTRA"
+        const val HISTORY_EXTRA_KEY = "HISTORY_EXTRA"
+        fun newInstance(bundle: Bundle): DetailFragment {
+            return DetailFragment().also {
+                it.arguments = bundle
+            }
+        }
+    }
+
+    private val viewModel: DetailsViewModel by lazy {
+        ViewModelProvider(this).get(DetailsViewModel::class.java)
+    }
+    private val movieId: Int by lazy {
+        arguments?.getInt(MOVIE_ID_KEY) ?: -1
+    }
+
+    private var _binding: DetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private var movieDetailsDTO: MovieDetailsDTO? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DetailFragmentBinding.inflate(inflater, container, false)
+
+        activity?.showHomeButton()
+
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel.liveDataDetails.observe(viewLifecycleOwner,
+            {
+                binding.apply {
+                    when (it) {
+                        is AppState.Loading -> {
+                            errorFrame.errorContainer.hide()
+                            loadingFrame.loadingContainer.show()
+                        }
+                        is AppState.Success<*> -> {
+                            errorFrame.errorContainer.hide()
+                            loadingFrame.loadingContainer.hide()
+                            movieDetailsDTO = it.data as MovieDetailsDTO
+                            fillDetail()
+                        }
+                        is AppState.Error -> {
+                            errorFrame.errorContainer.show()
+                            loadingFrame.loadingContainer.hide()
+                            showError(it.t)
+                        }
+                    }
+                }
+            })
+
+        viewModel.getMovieDetailsFromRemoteSource(movieId)
+    }
+
+    private fun fillDetail() {
+        movieDetailsDTO?.let {
+            val releaseYear = it.release_date?.subSequence(0, 4).toString()
+            binding.apply {
+                title.text = it.title
+                originalTitle.text = resources.getString(R.string.detail_orig_title_pattern)
+                    .getStringFormat(it.original_title, releaseYear)
+                image.load("${DETAILS_URL_BASE}${it.poster_path}")
+                genres.text = it.genres?.let { getGenresNames(it) }
+                duration.text = resources.getString(R.string.detail_duration_pattern)
+                    .getStringFormat(
+                        it.runtime,
+                        resources.getString(R.string.minute),
+                        it.runtime?.getFormatDuration()
+                    )
+                rating.text = resources.getString(R.string.detail_rating_pattern)
+                    .getStringFormat(it.vote_average, it.vote_count)
+                budget.text = resources.getString(R.string.detail_budget_pattern)
+                    .getStringFormat(resources.getString(R.string.budget), it.budget)
+                revenue.text = resources.getString(R.string.detail_revenue_pattern)
+                    .getStringFormat(resources.getString(R.string.revenue), it.revenue)
+                released.text = resources.getString(R.string.detail_release_date_pattern)
+                    .getStringFormat(
+                        resources.getString(R.string.released),
+                        it.release_date?.formatDate()
+                    )
+                overview.text = it.overview
+            }
+        }
+    }
+
+    private fun getGenresNames(genres: Array<GenresDTO.GenreDTO>): String {
+        val sb = StringBuilder()
+        for (i in genres.indices) {
+            sb.append(genres[i].name)
+            if (i != genres.lastIndex) sb.append(", ")
+        }
+        return sb.toString()
+    }
+
+    private fun showError(t: Throwable) {
+        binding.apply {
+            errorFrame.errorMessage.text = resources.getString(R.string.error_message_pattern)
+                .getStringFormat(
+                    resources.getString(R.string.error),
+                    t.message
+                )
+            errorFrame.errorActionButton.setOnClickListener {
+                viewModel.getMovieDetailsFromRemoteSource(movieId)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
